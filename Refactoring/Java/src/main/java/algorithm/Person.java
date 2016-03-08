db46eb6f1ce56224cb89da832135d6ab1a6d6c83
Amitai Schlair <aschlair@pillartechnology.com>
2016-03-08 09:52:52 -0500

    Make Person's name and birthDate private.

--- Refactoring/Java/src/main/java/algorithm/Person.java
+++ Refactoring/Java/src/main/java/algorithm/Person.java
@@ -3,8 +3,8 @@ package algorithm;
 import java.util.Date;
 
 public class Person {
-	public String name;
-	public Date birthDate;
+	private String name;
+	private Date birthDate;
 	
 	public Person(String name, Date birthDate) {
 		this.name = name;
