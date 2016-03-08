edf593474ef62cd5d29fe8bfc5d7855a38aa91d5
Amitai Schlair <aschlair@pillartechnology.com>
2016-03-08 09:54:04 -0500

    Person.name isn't used anywhere.

--- Refactoring/Java/src/main/java/algorithm/Person.java
+++ Refactoring/Java/src/main/java/algorithm/Person.java
@@ -3,20 +3,12 @@ package algorithm;
 import java.util.Date;
 
 public class Person {
-	private String name;
 	private Date birthDate;
 	
 	public Person(String name, Date birthDate) {
-		this.name = name;
 		this.birthDate = birthDate;
 	}
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	public Date getBirthDate() {
 		return birthDate;
 	}
