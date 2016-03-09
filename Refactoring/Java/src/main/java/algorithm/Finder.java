3997646cb0c9ebf8472f36a3e519145fa528a655
Amitai Schlair <aschlair@pillartechnology.com>
2016-03-09 08:45:40 -0500

    Rename variables in Finder.

--- Refactoring/Java/src/main/java/algorithm/Finder.java
+++ Refactoring/Java/src/main/java/algorithm/Finder.java
@@ -5,35 +5,35 @@ import java.util.List;
 public class Finder {
 	private final List<Person> listOfPersons;
 
-	public Finder(List<Person> p) {
-		listOfPersons = p;
+	public Finder(List<Person> listOfPersons) {
+		this.listOfPersons = listOfPersons;
 	}
 
-	public PersonPair find(FindDistance ft) {
-		List<PersonPair> tr = new ArrayList<PersonPair>();
+	public PersonPair find(FindDistance findDistanceThingy) {
+		List<PersonPair> listOfPairsOfPersons = new ArrayList<PersonPair>();
 
 		for (int i = 0; i < listOfPersons.size() - 1; i++) {
 			for (int j = i + 1; j < listOfPersons.size(); j++) {
-				PersonPair r = new PersonPair();
+				PersonPair thisPersonPair = new PersonPair();
 				if (listOfPersons.get(i).getBirthDate().getTime() < listOfPersons.get(j).getBirthDate().getTime()) {
-					r.personOne = listOfPersons.get(i);
-					r.personTwo = listOfPersons.get(j);
+					thisPersonPair.personOne = listOfPersons.get(i);
+					thisPersonPair.personTwo = listOfPersons.get(j);
 				} else {
-					r.personOne = listOfPersons.get(j);
-					r.personTwo = listOfPersons.get(i);
+					thisPersonPair.personOne = listOfPersons.get(j);
+					thisPersonPair.personTwo = listOfPersons.get(i);
 				}
-				r.millisecondsBetweenDates = r.personTwo.getBirthDate().getTime() - r.personOne.getBirthDate().getTime();
-				tr.add(r);
+				thisPersonPair.millisecondsBetweenDates = thisPersonPair.personTwo.getBirthDate().getTime() - thisPersonPair.personOne.getBirthDate().getTime();
+				listOfPairsOfPersons.add(thisPersonPair);
 			}
 		}
 
-		if (tr.size() < 1) {
+		if (listOfPairsOfPersons.size() < 1) {
 			return new PersonPair();
 		}
 
-		PersonPair answer = tr.get(0);
-		for (PersonPair result : tr) {
-			switch (ft) {
+		PersonPair answer = listOfPairsOfPersons.get(0);
+		for (PersonPair result : listOfPairsOfPersons) {
+			switch (findDistanceThingy) {
 				case CLOSEST :
 					if (result.millisecondsBetweenDates < answer.millisecondsBetweenDates) {
 						answer = result;
