17436cf5f75090aac2db2d5d7336caa60abf908d
Amitai Schlair <aschlair@pillartechnology.com>
2016-03-11 19:03:23 -0500

    Belatedly add PersonList.

---
+++ Refactoring/Java/src/main/java/algorithm/PersonList.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonList {
+	private List<Person> persons;
+
+	public PersonList(Person...persons) {
+		this.persons = new ArrayList<Person>();
+		for (Person person : persons) {
+			this.persons.add(person);
+		}
+	}
+	
+	public int size() {
+		return persons.size();
+	}
+	
+	public Person get(int index) {
+		return persons.get(index);
+	}
+
+}
