664d1be3c44632a7de79548f817545a5bf288cd3
Amitai Schlair <aschlair@pillartechnology.com>
2016-03-29 15:50:24 -0400

    Replace Calendar with GregorianCalendar.

--- Refactoring/Java/src/main/java/com/schmonz/birthdaymath/Person.java
+++ Refactoring/Java/src/main/java/com/schmonz/birthdaymath/Person.java
@@ -1,16 +1,12 @@
 package com.schmonz.birthdaymath;
 
-import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
 	private long birthTime;
 
 	public Person(int year, int monthIndexedFrom1, int dayIndexedFrom1) {
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.YEAR, year);
-		cal.set(Calendar.MONTH, monthIndexedFrom1 - 1);
-		cal.set(Calendar.DAY_OF_MONTH, dayIndexedFrom1);
-		this.birthTime = cal.getTime().getTime();
+		this.birthTime = new GregorianCalendar(year, monthIndexedFrom1 - 1, dayIndexedFrom1).getTimeInMillis();
 	}
 
 	public long getBirthTime() {
